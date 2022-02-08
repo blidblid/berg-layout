8e248dfaf0015da61a2021485e5b1be502b1a018
Albin Berglund <e.a.e.berglund@gmail.com>
2022-02-08 21:46:05 +0100

    test panel alignment (#2)

--- projects/angular/karma.conf.js
+++ projects/angular/karma.conf.js
@@ -37,5 +37,11 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu'],
+      },
+    },
   });
 };
