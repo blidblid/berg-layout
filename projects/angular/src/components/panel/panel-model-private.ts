bc22b36f70816451f82fb9b1ecf7a007a9fa4ebe
Albin <e.a.e.berglund@gmail.com>
2022-02-11 22:57:35 +0100

    chore

--- projects/angular/src/components/panel/panel-model-private.ts
+++ projects/angular/src/components/panel/panel-model-private.ts
@@ -5,12 +5,6 @@ export const BERG_RESIZE_EXPAND_DEBOUNCE = 10;
 
 export const BERG_RESIZE_TWO_DIMENSION_COLLECTION_DISTANCE = 8;
 
-export const BERG_RESIZE_RESIZING_CLASS_NAME =
-  'berg-panel-resize-toggle-resizing';
-
-export const BERG_RESIZE_PREVIEWING_CLASS_NAME =
-  'berg-panel-resize-toggle-previewing';
-
 export interface BergPanelResizeSize {
   rect: DOMRect;
   width?: number;
