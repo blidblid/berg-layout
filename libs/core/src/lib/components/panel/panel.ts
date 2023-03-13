04b431d116c455de9d578373611148c373336fd7
Albin Berglund <e.a.e.berglund@gmail.com>
2023-03-13 17:30:53 +0100

    Default layout element (#21)
    * fix: add default layout element
    
    * chore: bump
    
    * fix: set z-index on content

--- libs/core/src/lib/components/panel/panel.ts
+++ libs/core/src/lib/components/panel/panel.ts
@@ -56,7 +56,7 @@ import { validateOutputBindingMode, validateSlot } from './panel-util-private';
 
 export class BergPanelElement extends WebComponent<BergPanelInputs> {
   private backdropElement: HTMLElement;
-  private layout: BergLayoutElement;
+  private layout = document.createElement('berg-layout') as BergLayoutElement;
 
   private resizeToggle$ = this.changes.slot.pipe(
     map((slot) => this.layout.resizeToggles[slot])
@@ -543,8 +543,8 @@ export class BergPanelElement extends WebComponent<BergPanelInputs> {
   }
 
   connectedCallback(): void {
-    this.temporarilyDisableTransitions();
     this.layout = this.findLayoutElement();
+    this.temporarilyDisableTransitions();
 
     this.classList.add(BERG_PANEL_CLASS);
     this.subscribeToResizing();
